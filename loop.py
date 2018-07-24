734c1e54c1760adf0a838d771544ebb3e4bac2ef
Richard Han (IS-TW) <richard_han@trend.com.tw>
2018-07-24 23:59:43 +0800

    add executed result into comments

--- loop.py
+++ loop.py
@@ -1,11 +1,23 @@
 # Loop - range 5
 for x in range(5):
     print(x)
+"""
+     0 
+     1
+     2
+     3
+     4
+     5
+"""
 
 # Loop - Lists
 fruits = ['apple', 'orange']
 for fruit in fruits:
     print(fruit)
+"""
+apple
+orange
+"""
 
 # Loop - Dictionaries
 countries = {
@@ -14,21 +26,29 @@ countries = {
 }
 for key, value in countries.items():
     print('%s: %s' % (key, value))
+"""
+TW: Taiwan
+PH: Philippines
+"""
 
 # While Loop
 x = 0
-while x < 10:
+while x < 3:
     print (x)
     x += 1
+"""
+0
+1
+2
+"""
 
 # List Comprehensions
-#  %: Modulus operator, it is used for remainder division on integers
-odds = [ x for x in range(10) if x % 2]
-print ("odds:")
-print ([ x for x in range(10) if x % 2])
-
+#  %:  Modulus operator, it is used for remainder division on integers
 odds = []
 for x in range (10):
     if x % 2:
         odds.append(x)
 print (odds)
+"""
+[1, 3, 5, 7, 9]
+"""
