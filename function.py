d5af5f7cb512a059e3087caa66951e4ba3cefb9a
Richard Han (IS-TW) <richard_han@trend.com.tw>
2018-07-25 02:02:00 +0800

    rename function name to recursion_fib, also remove one example.

--- function.py
+++ function.py
@@ -22,17 +22,6 @@ Yo!!
 YoYo!!!
 """
 
-def fun_display_myname(fname):
-    """  Display input unction Documentation"""
-    print("My name is " + fname)
-
-fun_display_myname("Richard")
-fun_display_myname("RJ")
-"""
-My name is Richard
-My name is RJ
-"""
-
 def write_log(message, prefix='DefaultPrefix'):
     """  Positional  + Keyword """
     print ('[%s] %s' % (prefix , message))
@@ -40,7 +29,6 @@ def write_log(message, prefix='DefaultPrefix'):
 write_log('Application start', 'Main Function')
 write_log('Login  start', 'Login Function')
 write_log('Debug Information')
-
 """
 [Main Function] Application start
 [Login Function] Login  start
@@ -49,13 +37,18 @@ write_log('Debug Information')
 
 # Fibonacci Sequence
 # 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597 ...
-def fib(n):
+
+def recursion_fib(n):
     if n == 0: return 0
     elif n == 1: return 1
-    else: return fib(n-1)+fib(n-2)
+    else: return recursion_fib(n-1)+recursion_fib(n-2)
 
-for x in range(10):
-    print 'x=' + str(x) + ' fib=' + str(fib(int(x)))
+print recursion_fib(10)
+"""
+55
+"""
+for i in range(10):
+    print 'i=' + str(i) + ' fib=' + str(recursion_fib(int(i)))
 """
 x=0 fib=0
 x=1 fib=1
